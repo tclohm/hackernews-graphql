08c9de36bcae15eb5141e75b33ca8f44ba76d15e
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-07-04 14:27:14 -0700

    added update functionality on index

--- src/index.js
+++ src/index.js
@@ -1,64 +1,69 @@
 const { GraphQLServer } = require('graphql-yoga');
+const { PrismaClient } = require('@prisma/client')
 
 // graphQL schema, defines simple query called info
 // schema has three special root types: query, mutation, subscription
 // const typeDefs;
 
-let links = [{
-		id: 'link-0',
-		url: 'www.youtube.com',
-		description: 'heaps of bad videos'
-	},
-	{
-		id: 'link-1',
-		url: 'www.vimeo.com',
-		description: 'better quality videos'
-	},
-	{
-		id: 'link-2',
-		url: 'www.netflix.com',
-		description: 'industry shaker'
-	}
-];
-
 // actual implementation of graphQL schema
 // structure is identical of type definition inside typeDefs: Query.info
-let idCount = links.length
 const resolvers = {
 	Query: {
 		info: () => `This is the API of a Hackernews Clone`,
-		feed: () => links,
-		link: (parent, args) => links.find(link => args.id == link.id)
+		// context arg entire prisma client instance
+		feed: async (parent, args, context) => {
+			return context.prisma.link.findMany()
+		},
+		link: (parent, args, context) => {
+			return context.prisma.link.findOne({
+				where: {
+					id: Number(args.id)
+				}
+			})
+		}
 	},
 	Mutation: {
-		post: (parent, args) => {
-			const link = {
-				id: `link-${idCount++}`,
-				description: args.description,
-				url: args.url,
-			}
-			links.push(link)
+		post: (parent, args, context, info) => {
+			const newLink = context.prisma.link.create({
+				data: {
+					url: args.url,
+					description: args.description,
+				},
+			})
+			return newLink
+		},
+		updateLink: (parent, args, context, info) => {
+			const found = context.prisma.link.findOne({
+				where: { id: Number(args.id) }
+			});
+
+			const link = context.prisma.link.update({
+				where: { id: Number(args.id) },
+				data: {
+					url: args.url ? args.url : found.url,
+					description: args.description ? args.description : found.description
+				}
+			})
 			return link
 		},
-		updateLink: (parent, args) => {
-			let link = links.find(link => link.id == args.id)
-			link.url = (args.url == undefined) ? link.url : args.url
-			link.description = (args.description == undefined) ? link.description : args.description 
-			return link
-		},
-		deleteLink: (parent, args) => {
-			let link = links.find(el => el.id == args.id)
-			let index = links.indexOf(link)
-			links.splice(index, 1)
+		deleteLink: (parent, args, context, info) => {
+			const link = context.prisma.link.delete({
+				where: { id: Number(args.id) },
+			})
 			return link
 		}
 	}
 };
 
+const prisma = new PrismaClient()
+
 // schema and resolvers are bundled and passed to the GraphQLServer which is imported from graphql-yoga
 const server = new GraphQLServer({
 	typeDefs: './src/schema.graphql',
-	resolvers
+	resolvers,
+	context: {
+		prisma,
+	}
 });
 
 server.start(() => console.log(`🚀 Server is running on 4000`))
--- src/script.js
+++ src/script.js
@@ -3,6 +3,12 @@ const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
 
 async function main() {
+	const newLink = await prisma.link.create({
+		data: {
+			description: 'networks middleman',
+			url: 'www.hulu.com',
+		},
+	})
 	const allLinks = await prisma.link.findMany()
 	console.log(allLinks)
 }
